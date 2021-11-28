99caecefd6d8de26f7af77144dfccc6605ac4ec6
Соловьев Роман <serrcel@gmail.com>
2021-11-28 18:03:43 +0400

    Added a skeleton for index file

---
+++ index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="Style.css">
+<script src="GoalsCreation.js"></script>
+<script src="PaymentMenu.js"></script>
+<script src="RegisterSkripts.js"></script>
+<title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
