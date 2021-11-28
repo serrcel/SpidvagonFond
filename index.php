092bbfb3cfb77514e90d47ecdced5f26a59b70e4
Соловьев Роман <serrcel@gmail.com>
2021-11-28 18:22:40 +0400

    Linked javascript files

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" type="text/css" href="Style.css">
 <script src="GoalsCreation.js"></script>
 <script src="PaymentMenu.js"></script>
 <script src="RegisterSkripts.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="Style.css">
 <title>Document</title>
 </head>
 <body>
