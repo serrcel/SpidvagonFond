c6e36238554be58019029da6b0581d24d6205545
Артём Лукьянов <a-lukyanov@bk.ru>
2021-11-29 14:55:26 +0400

    E-Mail fild been added to register form

--- RegisterSkripts.js
+++ RegisterSkripts.js
@@ -12,7 +12,7 @@ function CreateRegisterMenu()
 	let form = document.createElement('form');
 	form.method = "post";
 
-	form.innerHTML = '<h1>Регистрация</h1><label for="rUName">*Имя пользователя:</label><br><input type="text" id="rUName" name="UName" required minlength="2" maxlength="30"><br><label for="rPass">*Пароль:</label><br><input type="password" id="rPass" name="Pass" required minlength="5" maxlength="30"><br><label for="rPPass">*Повторите пароль:</label><br><input type="password" id="rPPass" name="PPass" required><br><label>Показать пароль <input type="checkbox" onclick="ShowPass()"></label><br><input type="submit" name="" id="reg"></input>'; 
+	form.innerHTML = '<h1>Регистрация</h1><label for="rUName">*Имя пользователя:</label><br><input type="text" id="rUName" name="UName" required minlength="2" maxlength="30"><br><label for="rPass">*Пароль:</label><br><input type="password" id="rPass" name="Pass" required minlength="5" maxlength="30"><br><label for="rPPass">*Повторите пароль:</label><br><input type="password" id="rPPass" name="PPass" required><br><label for="email">*Введите E-Mail:</label><br><input type="text" id="remail" name="email" required><br><label>Показать пароль <input type="checkbox" onclick="ShowPass()"></label><br><input type="submit" name="" id="reg"></input>';
 
 	document.body.appendChild(shadow);
 	shadow.appendChild(registerMenuBox);
