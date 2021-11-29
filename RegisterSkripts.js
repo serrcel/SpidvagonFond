9967e29736e1e8ad43351a99e01b4e0094cba32c
Артём Лукьянов <a-lukyanov@bk.ru>
2021-11-29 12:14:51 +0400

    basement of register form been created

---
+++ RegisterSkripts.js
@@ -0,0 +1,20 @@
+function CreateRegisterMenu()
+{
+	let shadow = document.createElement('div');
+	shadow.className = 'Shadow';
+	shadow.onclick=function()
+	{
+		shadow.remove();
+	}
+
+	let registerMenuBox = document.createElement('div');
+	registerMenuBox.className = "RegisterMenuBox";
+	let form = document.createElement('form');
+	form.method = "post";
+
+	form.innerHTML = '<h1>Регистрация</h1><label for="rUName">*Имя пользователя:</label><br><input type="text" id="rUName" name="UName" required minlength="2" maxlength="30"><br><label for="rPass">*Пароль:</label><br><input type="password" id="rPass" name="Pass" required minlength="5" maxlength="30"><br><label for="rPPass">*Повторите пароль:</label><br><input type="password" id="rPPass" name="PPass" required><br><label>Показать пароль <input type="checkbox" onclick="ShowPass()"></label><br><input type="submit" name="" id="reg"></input>'; 
+
+	document.body.appendChild(shadow);
+	shadow.appendChild(registerMenuBox);
+	registerMenuBox.appendChild(form);
+}
