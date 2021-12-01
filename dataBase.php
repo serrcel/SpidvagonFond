8e0cca78c4c82638ce0c96abe85aa05655493181
mrxantar <matiusrs@gmail.com>
2021-12-01 19:00:46 +0400

    added comments

--- dataBase.php
+++ dataBase.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-$ServerName = "localhost";
-$UserName = "root";
-$DataBase = "spidvagon";
+$ServerName = "localhost"; //Server Name
+$UserName = "root"; //User Name
+$DataBase = "spidvagon"; //Data Base Name
 $UPassword = "";
 
-$conn = new mysqli($ServerName, $UserName, $UPassword, $DataBase);
+$conn = new mysqli($ServerName, $UserName, $UPassword, $DataBase); //writing a connection to the conn variable
 
-if (mysqli_connect_errno())
+if (mysqli_connect_errno()) //checking the connection success
 {
 printf("Соединение не удалось: %s\n", mysqli_connect_error());
 exit();
