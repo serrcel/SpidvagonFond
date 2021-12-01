e5786cf24bd961ca035027a16591740832f09c06
mrxantar <matiusrs@gmail.com>
2021-12-01 18:48:14 +0400

    now function establishes connection with database

--- dataBase.php
+++ dataBase.php
@@ -1,3 +1,11 @@
 <?php
+session_start();
+$ServerName = "localhost";
+$UserName = "root";
+$DataBase = "spidvagon";
+$UPassword = "";
+
+$conn = new mysqli($ServerName, $UserName, $UPassword, $DataBase);
+
 
 ?>
